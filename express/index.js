ecea0338336cff4c6235930bbd250c2c5aa85bcf
Anomalous Leaf <exdragon01@gmail.com>
2022-06-27 02:12:41 +0800

    Refactored to separate web page initialisation into it's own function

--- express/index.js
+++ express/index.js
@@ -11,68 +11,8 @@ app.use(cors())
 
 app.get('/', async(req, res) => {
   try {
-    console.time()
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto("https://sinoalice.game-db.tw/nightmares");
-    await page.screenshot({ path: 'example.png' });
-
-    //Use puppeteer to display desired fields in table by clicking field button
-    const query = "Field";
-
-    //Run this  function in the console of the loaded page in puppeteer
-    page.evaluate(query => {
-      //Query to select all div elements that are descendants of the class "ctrlbtns"
-      const elements = [...document.querySelectorAll(".ctrlbtns div")];
-
-      // Either use .find or .filter, comment one of these
-      // find element with find
-      const targetElement = elements.find(e => e.innerText.includes(query));
-
-      // make sure the element exists, and only then click it
-      if (targetElement)
-      {
-        targetElement.click();
-      }
-
-      // Deselect all fields
-      // Get list of all enabled fields
-      const deselectElements = [...document.querySelectorAll(".dialogContent .sortBtn.large.enabled.en")];
-      console.log(deselectElements)
-
-      // Iterate through list and click each button to disable
-      deselectElements.forEach((element, index, array) => {
-        element.click();
-      })
-
-      // Get all deselected elements
-      const disabledElements = [...document.querySelectorAll(".dialogContent .sortBtn.large.en")];
-
-      // Then select only your desired fields (icon, colo skill, colo prepr time, colo duration time)
-      const selectedElements = disabledElements.filter(e => e.innerText.includes("Icon") || e.innerText.includes("Colo.Skill") || e.innerText.includes("Colo.Prep") || e.innerText.includes("Colo.Dur."))
-
-      // Iterate through list and click desired field
-      selectedElements.forEach((element, index, array) => {
-        element.click();
-      })
-
-      // Click the submit button to confirm selected fields. The class name is "dialogOK2"
-      const submitButton = [...document.querySelectorAll(".dialogOK2")];
-
-      submitButton[0].click()
-
-      
-    }, query);
+    const $ = await initialiseWebPage()
     
-    await page.screenshot({ path: 'example.png' });
-
-    const htmlString = await page.content();
-    const $ = cheerio.load(htmlString)
-
-    await browser.close();
-
-    console.timeEnd()
-
     const nightmareArray = [];
 
     const keys = [
@@ -126,7 +66,7 @@ app.get('/', async(req, res) => {
       // Find the skill active duration and get the time (class='colGvgSkillDur')
       element = $(parentElem).find('.colGvgSkillDur')
       nightmareDetails[keys[6]] = element.text();
-      
+
       nightmareArray.push(nightmareDetails);
     })
     
@@ -139,8 +79,76 @@ app.get('/', async(req, res) => {
     return res.status(404);
 
   }
-  //res.send('Hello World!')
 })
+
+// Function to initialise loaded fields in browser (Preparation for web scraping)
+// Need to select the fields to display and get the html loaded in cheerio
+async function initialiseWebPage()
+{
+  console.time()
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://sinoalice.game-db.tw/nightmares");
+  await page.screenshot({ path: 'example.png' });
+
+  //Use puppeteer to display desired fields in table by clicking field button
+  const query = "Field";
+
+  //Run this  function in the console of the loaded page in puppeteer
+  page.evaluate(query => {
+    //Query to select all div elements that are descendants of the class "ctrlbtns"
+    const elements = [...document.querySelectorAll(".ctrlbtns div")];
+
+    // Either use .find or .filter, comment one of these
+    // find element with find
+    const targetElement = elements.find(e => e.innerText.includes(query));
+
+    // make sure the element exists, and only then click it
+    if (targetElement)
+    {
+      targetElement.click();
+    }
+
+    // Deselect all fields
+    // Get list of all enabled fields
+    const deselectElements = [...document.querySelectorAll(".dialogContent .sortBtn.large.enabled.en")];
+    console.log(deselectElements)
+
+    // Iterate through list and click each button to disable
+    deselectElements.forEach((element, index, array) => {
+      element.click();
+    })
+
+    // Get all deselected elements
+    const disabledElements = [...document.querySelectorAll(".dialogContent .sortBtn.large.en")];
+
+    // Then select only your desired fields (icon, colo skill, colo prepr time, colo duration time)
+    const selectedElements = disabledElements.filter(e => e.innerText.includes("Icon") || e.innerText.includes("Colo.Skill") || e.innerText.includes("Colo.Prep") || e.innerText.includes("Colo.Dur."))
+
+    // Iterate through list and click desired field
+    selectedElements.forEach((element, index, array) => {
+      element.click();
+    })
+
+    // Click the submit button to confirm selected fields. The class name is "dialogOK2"
+    const submitButton = [...document.querySelectorAll(".dialogOK2")];
+
+    submitButton[0].click()
+
+    
+  }, query);
+  
+  await page.screenshot({ path: 'example.png' });
+
+  const htmlString = await page.content();
+  const $ = cheerio.load(htmlString)
+
+  await browser.close();
+
+  console.timeEnd()
+
+  return $;
+}
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
