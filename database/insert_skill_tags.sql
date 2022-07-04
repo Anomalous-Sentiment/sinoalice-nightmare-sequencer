2cd50abd85263953b95d932bb859a7ada7d58e20
Anomalous Leaf <exdragon01@gmail.com>
2022-07-05 00:13:54 +0800

    All tag relations added. Inserts also modified to ignore on conflict

--- database/insert_skill_tags.sql
+++ database/insert_skill_tags.sql
@@ -1,69 +1,49 @@
-INSERT INTO skill_tag_relations (tag_id, jp_colo_skill_name)
+
+-- Initialise element_attributes table
+INSERT INTO element_attributes (attribute_id, attribute)
 VALUES
-    (2, '偽リハ堅牢ナ鎧トナル'), -- False robustness, pdef buff
-    (0, '偽リハ鋭利ナ刃トナル'), -- False blade, patk buff
-    (0, '唱歌ハ必勝ノ刃トナル'), -- Victorious melody, patk buff
-    (1, '唱歌ハ必勝ノ刃トナル'), -- Victorious melody, matk buff
-    (19, '偽リハ武具ヲ破壊スル'), -- Smash the fake weapon, patk debuff
-    (21, '偽リハ鎧ヲ破壊スル'), -- False destruction, pdef debuff
-    (15, '叫声ハ炎熱ノ壁トナル'), -- Engulfing shout, reduce fire
-    (14, '号令ハ疾風ノ加護トナル'), -- Gale blessing, wind buff
-    (15, '呟キハ流水ノ壁トナル'), -- Cry of the damned, reduce water
-    (15, '命令ハ疾風ノ壁トナル'), -- Soothing command, reduce wind
-    (16, '咆哮ハ反撃ノ狼煙トナル'), -- Defiant roar, disadvantage
-    (0, '唱歌ハ忠義ノ刃トナル'), -- Song of camaraderie, patk buff
-    (1, '唱歌ハ忠義ノ刃トナル'), -- Song of camaraderie, matk buff
-    (0, '唱歌ハ裏切リノ刃トナル'), -- Song of betrayal, patk buff
-    (1, '唱歌ハ裏切リノ刃トナル'), -- Song of betrayal, matk buff
-    (18, '嘆息ハ援護ヲ妨ゲル'), -- Hindering wail, support skill rate down
-    (14, '囁キハ流水ノ加護トナル'), -- Aqueous blessing, water buff
-    (4, '妖精王ハ魔力ヲ供給スル'), -- Fairy king magic, reduce sp
-    (14, '怒号ハ炎熱ノ加護トナル'), -- Combustive blessing, fire buff
-    (19, '怨嗟ハ刃ヲ脆ク鈍ラセル'), -- Begrudging blade, patk reduce
-    (20, '怨嗟ハ刃ヲ脆ク鈍ラセル'), -- Begrudging blade, matk reduce
-    (19, '怨嗟ハ剣ヲ脆ク鈍ラセル'), -- Begrudging sword, patk reduce
-    (20, '怨嗟ハ剣ヲ脆ク鈍ラセル'), -- Begrudging sword, matk reduce
-    (21, '慟哭ハ砦ヲ縛ル枷トナル'), -- Fort of lamentation, pdef reduce
-    (22, '慟哭ハ砦ヲ縛ル枷トナル'), -- Fort of lamentation, mdef reduce
-    (21, '慟哭ハ鎧ヲ縛ル枷トナル'), -- Armor of lamentation, pdef reduce
-    (22, '慟哭ハ鎧ヲ縛ル枷トナル'), -- Armor of lamentation, mdef reduce
-    (23, '晩鐘ハ再戦ノ烽煙/盾'), -- Rematch at the last bell/shield, gear reset
-    (23, '晩鐘ハ再戦ノ烽煙/矛'), -- Rematch at the last bell/spear, gear reset
-    (23, '晩鐘ハ再戦ノ烽煙トナル'), -- Smoke signals a rechallenge, gear reset
-    (17, '歓声ハ援護ノ加護トナル'), -- Uplifting chant, support skill rate up
-    (4, '水精ハ魔力ヲ供給スル'), -- Tidal fairy magic, sp reduce
-    (4, '水精ハ魔力ヲ掌握スル'), -- Tidal fairy master, sp reduce
-    (4, '炎精ハ魔力ヲ供給スル'), -- Infernal fairy magic, sp reduce
-    (2, '祝福ハ堅固ナ鎧トナル'), -- Blessed armour, pdef buff
-    (3, '祝福ハ堅固ナ鎧トナル'), -- Blessed armour, mdef buff
-    (4, '風精ハ魔力ヲ供給スル'), -- Zephyr fairy magic, sp reduce
-    (2, '祝福ハ強固ナ鎧トナル'), -- Enchanted armor, pdef buff
-    (3, '祝福ハ強固ナ鎧トナル'), -- Enchanted armor, mdef buff
-    (12, '血界ハ楽器ヲ拒ム'), -- World of silence, instru wep effect down
-    (4, '炎精ハ魔力ヲ掌握スル'), -- Infernal fairy master, sp reduce
-    (4, '風精ハ魔力ヲ掌握スル'), -- Zephyr fairy mastery (master)?, sp reduce
-    (12, '血界ハ魔書ヲ拒ム'), -- World of ignorance, tome wep effect reduce
-    (12, '血界ハ祈祷ヲ拒ム'), -- World of order, staff wep effect reduce
-    (10, '警鐘ハ散華ノ飛沫ヲ呼ブ'), -- For whom the bell splashes, water bell
-    (5, '逆巻ク魔力ノ追風'), -- Reversal magic, sp recover
-    (0, '真実ハ魔術ノ贄トナル'), -- Sacrifice of truth
-    (0, '逆巻ク魔力ノ順風'), -- Magic order
-    (0, '警鐘ハ散華ノ風ヲ呼ブ'), -- Bell souds a storm, wind bell
-    (0, '血界ハ斬撃ヲ拒ム'), -- Rejecting the slash
-    (0, '真実ハ魔壁ヲ破壊スル'), -- Destruction of truth
-    (0, '真実ハ魔術ヲ打チ消ス'), -- Negating truth
-    (0, '血界ハ衝撃ヲ拒ム'), -- Rejecting the charge
-    (0, '警鐘ハ散華ノ火花ヲ呼ブ'), -- The bell souds a wildfire, fire bell
-    (0, '慟哭ハ砦ヲ縛ル枷トナル'), -- Fort of lamentation
-    (0, '解キ放ツ魔力ノ奔流'), -- Magical torrent
-    (0, '真実ハ魔壁ノ贄トナル'), -- Wall sacrifice
-    (0, '聖霊ハ逆刻ノ鐘ヲ鳴ラス'), -- Spiritual reverse
-    (0, '祝福ハ堅牢ナ鎧トナル'), -- Blessed fort
-    (0, '祖ハ順刻ノ魔笛ヲ鳴ラス'), -- Magic flute's whistle
-    (0, '訓令ハ銃撃ノ加護トナル'), -- Order id a boon in gunplay
-    (0, '鼓舞ハ剣戟ノ加護トナル'), -- Insight is a boon in swordplay
-    (0, '魔鏡ハ敵手ヲ影写スル'), -- Malice mirrored
-    (0, '黒キ魔獣ハ憎悪ニ吼エル'), -- Roar of the black mabeat's hatred
-    (0, '紫煙ハ瞬刻ヲ告ゲル'), -- Haze heralds the moment
-    (0, '黒煙ハ鈍重ナ呪縛トナル'), -- Smoke dulled malediction
-    (0, '');
+    (1, 'Fire'),
+    (2, 'Water'),
+    (3, 'Wind')
+ON CONFLICT (attribute_id) DO NOTHING;
+
+
+-- Initialise rarities table
+INSERT INTO rarities (rarity_id, rarity)
+VALUES
+    (3, 'A'),
+    (4, 'S'),
+    (5, 'SS/SR'),
+    (6, 'L')
+ON CONFLICT (rarity_id) DO NOTHING;
+
+
+-- Initialise tags table
+INSERT INTO tags (tag_id, tag, description)
+VALUES
+    (0, 'P.Atk Buff', 'Buffs or increases physical attack'),
+    (1, 'M.Atk Buff', 'Buffs or increases magical attack'),
+    (2, 'P.Def Buff', 'Buffs or increases physical defense'),
+    (3, 'M.Def Buff', 'Buffs or increases magical defense'),
+    (4, 'SP Reduction', 'Reduces SP usage'),
+    (5, 'SP Recovery', 'Recovers SP'),
+    (6, 'Buff', 'Buffs or increases one or more stats'),
+    (7, 'Debuff', 'Debuffs or decreases one or more stats'),
+    (8, 'Elemental Bell', 'Increases element effectiveness and increases damage from opposing element'),
+    (9, 'Fire Bell', 'Increases fire weapon effectiveness and increases damage received from water weapons'),
+    (10, 'Water Bell', 'Increases water weapon effectiveness and increases damage received from wind weapons'),
+    (11, 'Wind Bell', 'Increases wind weapon effectiveness and increases damage received from fire weapons'),
+    (12, 'Reduce Weapon Effect', 'Decreases effectiveness of a weapon type'),
+    (13, 'Increase Weapon Effect', 'Increases effectiveness of a weapon type'),
+    (14, 'Elemental Buff', 'Increases effectiveness of weapons of a specified element'),
+    (15, 'Elemental Debuff', 'Decreases effectiveness of weapons of a specified element'),
+    (16, 'Disadvantage', 'Effective only when at a disadvantage'),
+    (17, 'Support Skill Rate Up', 'Increases activation rate of colosseum support skills'),
+    (18, 'Support Skill Rate Down', 'Decreases activation rate of colosseum support skills'),
+    (19, 'P.Atk Debuff', 'Debuffs or decreases physical attack'),
+    (20, 'M.Atk Debuff', 'Debuffs or decreases magical attack'),
+    (21, 'P.Def Debuff', 'Debuffs or decreases physical defense'),
+    (22, 'M.Def Debuff', 'Debuffs or decreases magical defense'),
+    (23, 'Gear Reset', 'Increases number of times players can change gear sets')
+ON CONFLICT (tag_id) DO NOTHING;
+
