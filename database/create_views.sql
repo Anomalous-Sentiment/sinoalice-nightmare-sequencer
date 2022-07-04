c8e803ac5710a2ec22f67886dbf63ccdc986f935
Anomalous Leaf <exdragon01@gmail.com>
2022-07-04 18:42:59 +0800

    Added new view which gives all nightmares and their associated tags, whether it has a tag or not. And if it has no tag, set the applied_tags column to an empty array. (Without the ARRAY_REMOVE() function, it is NULL by default due to the LEFT JOIN)

--- database/create_views.sql
+++ database/create_views.sql
@@ -1,3 +1,4 @@
+-- Join tables to see nightmares that have tags applied to them, and aggregate all tags in the applied_tags column of view
 CREATE OR REPLACE VIEW nightmaresWithTags AS
     SELECT nm.jp_name, nm.en_name, nm.jp_icon_url, nm.en_icon_url, attr.attribute, pure_skills.jp_colo_skill_name, combined_skills.jp_colo_skill_desc, pure_skills.en_colo_skill_name,combined_skills.en_colo_skill_desc, rk.jp_rank, rk.en_rank, nm.global, ARRAY_AGG (tagTable.tag) applied_tags
     FROM nightmares nm
@@ -8,4 +9,17 @@ CREATE OR REPLACE VIEW nightmaresWithTags AS
     INNER JOIN pure_colo_skill_names pure_skills USING (jp_colo_skill_name)
     INNER JOIN skill_tag_relations tagRel USING (jp_colo_skill_name)
     INNER JOIN tags tagTable USING (tag_id)
+    GROUP BY nm.jp_name, nm.rarity_id, attr.attribute_id, combined_skills.jp_colo_skill_name, combined_skills.jp_rank, pure_skills.jp_colo_skill_name, rk.jp_rank, tagRel.jp_colo_skill_name;
+
+-- Same as above, joining all tables to see applied tags, but also include non-taggged nightmares
+CREATE OR REPLACE VIEW allNightmareDetails AS
+    SELECT nm.jp_name, nm.en_name, nm.jp_icon_url, nm.en_icon_url, attr.attribute, pure_skills.jp_colo_skill_name, combined_skills.jp_colo_skill_desc, pure_skills.en_colo_skill_name,combined_skills.en_colo_skill_desc, rk.jp_rank, rk.en_rank, nm.global, ARRAY_REMOVE(ARRAY_AGG (tagTable.tag), NULL) applied_tags
+    FROM nightmares nm
+    INNER JOIN rarities rare USING (rarity_id)
+    INNER JOIN element_attributes attr USING (attribute_id)
+    INNER JOIN colosseum_skills combined_skills USING (jp_colo_skill_name, jp_rank)
+    INNER JOIN ranks rk USING (jp_rank)
+    INNER JOIN pure_colo_skill_names pure_skills USING (jp_colo_skill_name)
+    LEFT JOIN skill_tag_relations tagRel USING (jp_colo_skill_name)
+    LEFT JOIN tags tagTable USING (tag_id)
     GROUP BY nm.jp_name, nm.rarity_id, attr.attribute_id, combined_skills.jp_colo_skill_name, combined_skills.jp_rank, pure_skills.jp_colo_skill_name, rk.jp_rank, tagRel.jp_colo_skill_name;
