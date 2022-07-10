591270c56a5683876dfc64092ea07fc0cd63c884
Anomalous Leaf <exdragon01@gmail.com>
2022-07-10 18:55:19 +0800

    Edited database view to give rarity id and attribute id as int

--- database/create_views.sql
+++ database/create_views.sql
@@ -16,9 +16,8 @@ CREATE OR REPLACE VIEW nightmaresWithTags AS
 DROP VIEW IF EXISTS allnightmaredetails;
 -- Same as above, joining all tables to see applied tags, but also include non-taggged nightmares and have tag if instead of tag name
 CREATE OR REPLACE VIEW allNightmareDetails AS
-    SELECT nm.jp_name, nm.en_name, nm.jp_icon_url, nm.en_icon_url, attr.attribute, pure_skills.jp_colo_skill_name, combined_skills.jp_colo_skill_desc, pure_skills.en_colo_skill_name,combined_skills.en_colo_skill_desc, rk.jp_rank, rk.en_rank, combined_skills.prep_time, combined_skills.effective_time, nm.global, ARRAY_REMOVE(ARRAY_AGG (tagTable.sub_tag), NULL) applied_tags, ARRAY_REMOVE(ARRAY_AGG (DISTINCT major.major_tag), NULL) major_tags, ARRAY_REMOVE(ARRAY_AGG(gen.general_tag), NULL) general_tags
+    SELECT nm.jp_name, nm.en_name, nm.jp_icon_url, nm.en_icon_url, nm.rarity_id, attr.attribute_id, pure_skills.jp_colo_skill_name, combined_skills.jp_colo_skill_desc, pure_skills.en_colo_skill_name,combined_skills.en_colo_skill_desc, rk.jp_rank, rk.en_rank, combined_skills.prep_time, combined_skills.effective_time, nm.global, ARRAY_REMOVE(ARRAY_AGG (tagTable.sub_tag), NULL) applied_tags, ARRAY_REMOVE(ARRAY_AGG (DISTINCT major.major_tag), NULL) major_tags, ARRAY_REMOVE(ARRAY_AGG(gen.general_tag), NULL) general_tags
     FROM nightmares nm
-    INNER JOIN rarities rare USING (rarity_id)
     INNER JOIN element_attributes attr USING (attribute_id)
     INNER JOIN colosseum_skills combined_skills USING (jp_colo_skill_name, jp_rank)
     INNER JOIN ranks rk USING (jp_rank)
