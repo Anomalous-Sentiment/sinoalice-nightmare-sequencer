98a00b1658f96529caa1f4783534e1d64d6efafd
Anomalous Leaf <exdragon01@gmail.com>
2022-07-14 19:09:53 +0800

    Merged frontend and backend

---
+++ sinoalice-nightmare-plotter/timedFunctions.js
@@ -0,0 +1,22 @@
+const { ToadScheduler, SimpleIntervalJob, Task } = require('toad-scheduler')
+const utility = require('./utilityFunctions')
+
+
+module.exports = 
+{
+    scheduleUpdates
+}
+
+function scheduleUpdates()
+{
+    const scheduler = new ToadScheduler()
+
+    const task = new Task('Update Database', () => { utility.updateDatabase() })
+    const job = new SimpleIntervalJob({ days: 1, }, task)
+
+    scheduler.addSimpleIntervalJob(job)
+
+    console.log('Updating job scheduled daily successfully')
+    // when stopping your app
+    //scheduler.stop()
+}
