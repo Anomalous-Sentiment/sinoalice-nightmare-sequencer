77e9faefcc67d00a21583139c10f56f96a62d1e7
Anomalous Leaf <exdragon01@gmail.com>
2022-07-14 23:35:47 +0800

    Modified about text again with exrta info

--- sinoalice-nightmare-plotter/TEXT_CONSTANTS.js
+++ sinoalice-nightmare-plotter/TEXT_CONSTANTS.js
@@ -1,6 +1,8 @@
 export const usageText = `I'm sure that the usage of this tool is quite self explanatory already, but I'll give a quick rundown of the features and usage. First, as you probably know, this is a nightmare planner for SINoALICE's colosseum game mode. All you need to do to use it is just click a nightmare and it'll be added to the timeline. Note that there is a restriction that nightmares will always be added where the previous nightmare's effect ends. And that is something that I don't plan on changing. This isn't intended to be some advanced tool where you can add multiple alternative nightmares as backup plans after all.
 
-Next, you can find the skill details of each nightmare by hovering over the image of the nightmare. They'll show as a tooltip.
+And I suspect that the high level guilds out there probably have their own dedicated master strategist with their own tools anyway, haha.
+
+So, continuing on, you can find the skill details of each nightmare by hovering over the image of the nightmare. They'll show as a tooltip.
 
 Another feature (or restriction, depending on how you see it) is that when you select a nightmare, all nightmares with the same skill are darkened and will not be able to be selected. This is because you cannot summon multiple nightmares with the same skill in colosseum. This is represents that in-game restriction. Of course, that restriction does not apply for certain special events, but we don't talk about that here ;)
 
@@ -21,33 +23,39 @@ And if you didn’t notice, there’s a specific colour scheme I’m using for n
 - Light Pink: Other Effects
 
 
-Another uhh, feature that was kinda added in half-heartedly was the miscellaneous statistics. Well, I won't say much about it, since I didn't know what would be useful as statistics, but can look at that if you're feeling bored I guess, haha.
+Another hmm... feature…? That was kinda added in was the miscellaneous statistics. Well, I won't say much about it, since I didn't know what would be useful as statistics, so I ended up throwing in whatever sounded useful. You can look at that if you're feeling bored I guess, haha.
 
-And finally, the main feature is the filtering and sorting functionality. You can check each tab for different categories of nightmares, and these categories can be further divided into sub tabs and then you can apply filters using the toggle buttons in those tabs. Hopefully, this will make it easier to search for nightmares by skill type (And maybe even find alternatives, haha).
+And finally, the main feature is the filtering functionality. You can check each tab for different categories of nightmares, and these categories can be further divided into sub tabs and then you can apply filters using the toggle switches in those tabs. One thing to note here is that the filtering works by showing only nightmares which have all of the selected tags. So keep that in mind if things suddenly disappear when you flip too many switches ;) Hopefully, this will make it easier to search for nightmares by skill type (And maybe even find alternatives, haha).
 
-Oh, right, and there's the ability to switch the view to the JP server nightmares too. All the functions mentioned above apply to the JP nightmares too, but their names and descriptions will be in Japanese. Well, the the tabs and filter options won't change though (*cough* I don't know Japanese *cough*).`
-export const developmentText = `The idea of creating this tool began with with finding Eucelia's Nightmare Planner tool. When I first saw it, I thought, that was a brilliant idea.
+Oh, right, and there's the ability to switch the view to the JP server nightmares too. All the functions mentioned above apply to the JP nightmares too, but their names and descriptions will be in Japanese. Well, the the tabs and filter option text won't change though (*cough* I don't know Japanese *cough*).
+
+Other features of note:
+- Vertically resizable chart, in case you have a loooong list of nightmares (Resize handles are a bit hard to see, but they’re right above the miscellaneous statistics button tab. There’s 3 of them. One on both corners, and in the middle)
+- Option to sort by rarity and element type
+- Meme 40 min colo option, in honour of our special 2nd anniversary treatment xD`
+
+export const developmentText = `The idea of creating this tool began with with finding Eucelia's Nightmare Planner. When I first saw it, I thought, that was a brilliant idea.
 
 But after playing around with it, I thought that there could be improvements that could be made to make it nicer to use. And since it seemed the developer no longer played the game, and since I thought this would be an interesting way to test out my own skills as a developer, I thought I'd try my hand at it.
 
 The main goals in the development of my own app, put simply, was a nicer looking timeline chart, and an improved categorisation and filtering system. Obviously, if I wasn't certain that I could improve on those 2 aspects, then there would be no point in starting anything. So to begin, I searched for libraries that would allow me to draw a graph or chart that could represent the 20 minutes spent in colosseum. And, this was, surprisingly, quite a challenge.
 
-When I first set out with my minuscule amount of knowledge, I only had one type of chart in mind: A gantt chart. I began my search by looking for libraries that could plot gantt charts, and although they existed, they were only suitable for time ranges between days, weeks, months, and years.
+When I first set out with my minuscule amount of knowledge, I only had one type of chart in mind: A gantt chart. The main reason I thought of a gantt chart was because I thought it fit the way nightmares worked. You can only summon one after another. Kinda like how one task depends on another one being finished first. It’s like a very… linear version of one. So I began my search by looking for libraries that could plot gantt charts, and although they existed, they were only suitable for time ranges between days, weeks, months, and years.
 And that was not suitable for my use case. So after many hours, and days so searching, I foud something that could be adapted for my use case: Google Charts.
 
 Specifically, the Timeline chart of Google Charts. It was almost exactly what I had been searching for, with only one problem: It only plotted timelines in chronological order. That is, from 0:00 to 20:00. And well, yes, that is quite logical, it makes sense. But in the context of SINoALICE's colosseum mode, it doesn't quite fit, since in colosseum, it's actually a timer that counts down from 20:00, so you'd need to do some mental calculations to line things up in realtime. Now, you've probably realised at this point if you've used my tool, but I never did end up fixing that.
 
 Yes. It proved to be too high of a hurdle for little old me to overcome, unfortunately.
 
-At any rate, looking at my first git commit, my work began on June 20, experimenting with the React Google Chart library and using the Timeline chart. After confirming that it worked as I expected it to, I then moved onto working on a way to retrieve data from the SINoALICE DB.
+At any rate, looking at my first git commit, my work began in proper on June 20, experimenting with the React Google Chart library and using the Timeline chart. After confirming that it worked as I expected it to, I then moved onto working on a way to retrieve data from the SINoALICE DB.
 
 For this, my initial thought was to use web scraping to obtain the data I needed, but after wading through about 3/4 of the web scraping code, I realised that there was in fact an API that I could use to get the data I needed without resorting to the incredible slog that was web scraping.
 
-So, I went and used it. Oh, but wait! Things never go as smoothly as they're planned! Of course, it is a law of life that things never go as planned. The next issue I ran into was... That the data from the API was not only missing english skill descriptions, but even some of the data was wrong! Some of the nightmare's EN skill names had the JP skill name instead! And that was among other inconsistency issues which I no longer remember.
+So, I went and used it. Oh, but wait! Things never go as smoothly as they're planned! Of course, it is a law of life that things never go as planned. The next issue I ran into was... That the data from the API was not only missing English skill descriptions, but even some of the data was wrong! Some of the nightmare's EN skill names had the JP skill name instead! And that was among other inconsistency issues which I no longer remember. Well, either way, it ended up creating a bit more work to do.
 
 But you might ask, among 600+ nightmares, how did I realise that there were issues with a select few nightmares? Did I go through them one by one? Well, honestly, I only realised because I got errors when I was inserting the data into my own database with foreign key violations.
 
-This brings me to my next stage of development. The database. Before I had realised that there were errors in the nightmare data, I designed a database for the sole purpose of tagging nightmares by skill, and storing that data so that it could be easily retrieved. Now, designing the database was probably the most enjoyable part of the whole development process. It's funny how that works, since one of the units I got the lowest marks for in uni was database systems, haha. But anyway, I drew up an ER diagram, with all the entities, foreign key relations and whatnot, and just went about implementing it.
+This brings me to my next stage of development. The database. Before I had realised that there were errors in the nightmare data, I designed a database for the sole purpose of tagging nightmares by skill, and storing that data so that it could be easily retrieved. Now, designing the database was probably the most enjoyable part of the whole development process. (And, by enjoyable, I mean least painful *cough*). It's funny how that works, since one of the units I got the lowest marks for in uni was database systems, haha. But anyway, I drew up an ER diagram, with all the entities, foreign key relations and whatnot, and just went about implementing it.
 
 Well, it sounds pretty simple, but it was actually quite hard to find a free database host that offered relational databases for use. In the end, I did find one and it was actually quite nice to use (offered PostgreSQL, which I never used before, but was quite similar to MySQL which I have used before). In implementing the database, one of the biggest issues I faced was actually something pretty stupid.
 
@@ -77,6 +85,6 @@ Now, I don't like making guarantees, but I'll try (keyword: try) to keep this up
 
 I was considering whether to leave contacts detail here (*cough* would be interesting to have an unknown developer *cough*), but then if there were issues, I wouldn't have any idea, so I guess I probably should... Unfortunately xD
 
-If there's any issues, can shoot me a message on Discord, I may... or may not try to resolve them. Feeling kinda burnt out to be honest, haha.
+If there's any issues, shoot me a message on Discord, I may... or may not try to resolve them. Feeling kinda burnt out to be honest, haha.
 
 And with this, I'll be returning to my life of having my job applications rejected. Hope you all have a good day, and if you read this wall of text to the end, thanks for hearing me out ^^`
