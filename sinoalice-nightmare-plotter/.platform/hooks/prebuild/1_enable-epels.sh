95a1a86765084c693d204de322b2c470a5dde108
Anomalous Leaf <exdragon01@gmail.com>
2022-07-16 13:58:00 +0800

    Committing deletions of old folder files

--- sinoalice-nightmare-plotter/.platform/hooks/prebuild/1_enable-epels.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-sudo amazon-linux-extras install epel -y
--- sinoalice-nightmare-plotter/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import nightmareReducer from './nightmaresSlice';
-
-export default configureStore({
-  reducer: {
-    nightmares: nightmareReducer
-  },
-})
