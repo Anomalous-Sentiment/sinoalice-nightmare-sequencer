455e1021d9b711023fe158cc672ca05045ea9171
Anomalous Leaf <exdragon01@gmail.com>
2022-07-16 13:35:00 +0800

    Renamed folders to reflect new tool name

---
+++ sinoalice-nightmare-sequencer/components/data-display.js
@@ -0,0 +1,190 @@
+import { Chart } from "react-google-charts";
+import Table from 'react-bootstrap/Table';
+import store from '../redux/store'
+
+export default function Statistics(props)
+{
+    const selectedNms = props.nightmares;
+
+    const fireNms = selectedNms.filter(nightmare => nightmare['applied_tags'].includes('Fire Bell') || nightmare['applied_tags'].includes('Fire Buff'))
+    const waterNms = selectedNms.filter(nightmare => nightmare['applied_tags'].includes('Water Bell') || nightmare['applied_tags'].includes('Water Buff'))
+    const windNms = selectedNms.filter(nightmare => nightmare['applied_tags'].includes('Wind Bell') || nightmare['applied_tags'].includes('Wind Buff'))
+    
+    let fireTime = 0;
+    let waterTime = 0;
+    let windTime = 0;
+    let firePrep = 0;
+    let waterPrep = 0;
+    let windPrep = 0;
+
+    // Calculate prep vs effective time (Sum total prep times and effective times)
+    let totalPrepTime = 0;
+    let totalEffectiveTime = 0;
+
+    selectedNms.forEach((nightmare, index, array) => {
+        let nmPrepTime = nightmare['prep_time'];
+        let nmActiveTime = nightmare['effective_time']
+
+        if (totalPrepTime + totalEffectiveTime + nmPrepTime > store.getState().nightmares.coloTime * 60)
+        {
+            //If adding this prep time will go over the time limit
+            let currentTotal = totalPrepTime + totalEffectiveTime + nmPrepTime;
+            let excess = currentTotal - store.getState().nightmares.coloTime * 60;
+
+            //Don't include prep time that goes over time limit.
+            nmPrepTime = nmPrepTime - excess;
+            //Prep time already over limit, so there is no effective active time
+            nmActiveTime = 0;
+        }
+        else if (totalPrepTime + totalEffectiveTime + nmPrepTime + nmActiveTime > store.getState().nightmares.coloTime * 60)
+        {
+            //If adding prep and active time onto total will go over time limit
+            let currentTotal = totalPrepTime + totalEffectiveTime + nmPrepTime + nmActiveTime;
+            let excess = currentTotal - store.getState().nightmares.coloTime * 60;
+
+            //Don't include the active time that goes over time limit
+            nmActiveTime = nmActiveTime - excess;
+        }
+
+        if (index > 0 && array[index - 1]['jp_colo_skill_name'] == '紫煙ハ瞬刻ヲ告ゲル')
+        {
+            //If previous nm skill was "Haze heralds the moment", set this nm prep time to 5 secs
+            nmPrepTime = 5;
+        }
+
+        if (nightmare['applied_tags'].includes('Fire Bell') || nightmare['applied_tags'].includes('Fire Buff'))
+        {
+            //Fire buff nm
+            fireTime = fireTime + nmActiveTime
+            firePrep = firePrep + nmPrepTime
+        }
+        else if (nightmare['applied_tags'].includes('Water Bell') || nightmare['applied_tags'].includes('Water Buff'))
+        {
+            // Water buff nm
+            waterTime = waterTime + nmActiveTime
+            waterPrep = waterPrep + nmPrepTime
+        }
+        else if (nightmare['applied_tags'].includes('Wind Bell') || nightmare['applied_tags'].includes('Wind Buff'))
+        {
+            //Wind buff nm
+            windTime = windTime + nmActiveTime
+            windPrep = windPrep + nmPrepTime
+        }
+
+        //Sum total prep and effective times regardless of nm type
+        totalPrepTime = totalPrepTime + nmPrepTime
+        totalEffectiveTime = totalEffectiveTime + nmActiveTime
+    })
+
+
+
+
+    const elementalData = [
+        ['Effective Elemental Time', 'Time in seconds'],
+        ['Effective Fire Elemental Time', fireTime],
+        ['Effective Water Elemental Time', waterTime],
+        ['Effective Wind Elemental Time', windTime]
+    ]
+
+    const prepVsEffectData = [
+        ['Preparation Time vs Effective Time', 'Time in seconds'],
+        ['Total Nightmare Prep Time', totalPrepTime],
+        ['Total Nightmare Effective Time', totalEffectiveTime]
+    ]
+
+    const elementalOptions = {
+        title: 'Elemental Time',
+        backgroundColor: '#e8e8e8',
+        colors: ['#ff0000', '#2e29cd', '#27bb1f'],
+        chartArea: {
+            width: '100%',
+            height: '100%'
+        }
+    }
+
+    const timeOptions = {
+        title: 'Preparation Time vs Effective Time',
+        backgroundColor: '#e8e8e8',
+        chartArea: {
+            width: '100%',
+            height: '100%'
+        }
+    }
+    // Calculate nightmare category composition??
+    return(
+        <div>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    <th>Times</th>
+                    <th>Total Time (sec)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>Total Preparation Time</td>
+                    <td>{totalPrepTime}</td>
+                    </tr>
+                    <tr>
+                    <td>Total Effective Time</td>
+                    <td>{totalEffectiveTime}</td>
+                    </tr>
+
+                </tbody>
+            </Table>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    <th>Elemental Type</th>
+                    <th>Number of Nightmares</th>
+                    <th>Total Preparation Time (sec)</th>
+                    <th>Total Effective Time (sec)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>Fire Elementals (Including bells)</td>
+                    <td>{fireNms.length}</td>
+                    <td>{firePrep}</td>
+                    <td>{fireTime}</td>
+                    </tr>
+                    <tr>
+                    <td>Water Elementals (Including bells)</td>
+                    <td>{waterNms.length}</td>
+                    <td>{waterPrep}</td>
+                    <td>{waterTime}</td>
+                    </tr>
+                    <tr>
+                    <td>Wind Elementals (including bells)</td>
+                    <td>{windNms.length}</td>
+                    <td>{windPrep}</td>
+                    <td>{windTime}</td>
+                    </tr>
+                </tbody>
+            </Table>
+            {props.nightmares.length > 0 &&
+            <div>
+                <div className="inline">
+                <Chart
+                chartType="PieChart"
+                data={prepVsEffectData}
+                options={timeOptions}
+                width={"100%"}
+                height={"300px"}
+                />
+                
+                <Chart
+                chartType="PieChart"
+                data={elementalData}
+                options={elementalOptions}
+                width={"100%"}
+                height={"300px"}
+                />
+                </div>
+
+            </div>
+
+            }
+        </div>
+    )
+}
---
+++ sinoalice-nightmare-sequencer/components/nightmare-image-list.js
@@ -0,0 +1,119 @@
+import { useState, memo, useMemo } from 'react';
+import ImageComponent from './image-component';
+import FilterBar from './filter-component';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from '../styles/ImageComponent.module.css'
+
+
+function NightmareImageList(props) {
+    const [appliedFilterList, setFilters] = useState([]);
+    const sortByList=[
+        <Dropdown.Item key='0' eventKey="0">Rarity (Low to High)</Dropdown.Item>,
+        <Dropdown.Item key='1' eventKey="1">Rarity (High to Low)</Dropdown.Item>,
+        <Dropdown.Item key='2' eventKey="2">Element</Dropdown.Item>,
+        <Dropdown.Item key='3' eventKey="3">Default</Dropdown.Item>
+      ]
+
+    function lowToHighRarity(a, b) {return a['rarity_id'] - b['rarity_id']};
+    function highToLowRarity(a, b) {return b['rarity_id'] - a['rarity_id']};
+    function element(a, b) {return a['attribute_id'] - b['attribute_id']};
+    function defaultOrder(a, b) {return 0};
+
+    function onSortingSelect(eventKey, event)
+    {
+      const sortingId = parseInt(eventKey);
+      
+      //Save  sorter in state (for maintaining sorting when image list is re-rendered)
+      setSorter(sortingFunctions[sortingId])
+    }
+
+    const sortingFunctions = [
+        ['Sorting By: Low to High rarity', lowToHighRarity],
+        ['Sorting By: High to Low rarity', highToLowRarity],
+        ['Sorting by: Element', element],
+        ['Sorting by: Default', defaultOrder]
+    ]
+
+    //Default sorter is default sorting
+    const [sorter, setSorter] = useState(sortingFunctions[3]);
+
+
+    const imageList = useMemo(() => props.list ? mapNightmaresToComponents(props.list) : [], [props.list, appliedFilterList, sorter])
+
+    function mapNightmaresToComponents (list)
+    {
+      //Apply filters to updated list
+      let newList = applyFilters(list)
+
+      //Apply sorting to filtereed list if selected
+      newList = newList.sort(sorter[1])
+    
+
+      //Map list elements to components
+      newList = newList.map((nightmare, index, arr) => {
+    
+        return (
+          <ImageComponent key={nightmare[props.displayOptions['icon']]} 
+          nightmare={nightmare} 
+          displayOptions={props.displayOptions} 
+          />
+        )
+    
+      })
+    
+      return newList;
+    }
+
+    function applyFilters(list)
+    {
+      //filter the nightmare list and update image list based on filtered nightmares
+      const filteredNms = list.filter(nm => appliedFilterList.every(filterTag => nm['applied_tags'].includes(filterTag)))
+
+      return filteredNms;
+    }
+
+
+  return (
+    <div>
+      <FilterBar filterList={props.filterList}
+      handleChange={setFilters}>
+      </FilterBar>
+      <DropdownButton 
+      id="dropdown-basic-button" 
+      title={sorter[0]}
+      menuVariant='dark'
+      onSelect={onSortingSelect}>
+        {sortByList}
+      </DropdownButton>
+      <div className={styles.grid}>
+        {imageList}
+      </div>
+    </div>
+
+  );
+}
+
+function areEqual(prevProps, nextProps)
+{
+  let isEqual = false;
+
+  //Check if old list is same as new list
+  if (prevProps.list && nextProps.list && prevProps.displayOptions && nextProps.displayOptions)
+  {
+    if (prevProps.displayOptions['icon'] == nextProps.displayOptions['icon'])
+    {
+      if (nextProps.list.length == prevProps.list.length)
+      {
+        isEqual= true
+      }
+    }
+
+  }
+
+
+  return isEqual;
+}
+
+export default memo(NightmareImageList, areEqual);
