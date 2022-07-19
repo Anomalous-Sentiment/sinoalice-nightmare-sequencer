e1dff5ef444251b69f8a4873610d44f7cd81e92f
Anomalous Leaf <exdragon01@gmail.com>
2022-07-19 18:32:04 +0800

    Fixed camelcase crossorign in head

--- sinoalice-nightmare-sequencer/pages/_document.js
+++ sinoalice-nightmare-sequencer/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7393687437464759"
-        crossorigin="anonymous"></script>
+        crossOrigin="anonymous"></script>
         </Head>
       <body>
         <Main />
