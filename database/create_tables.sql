9a7d024e01c792ddc7670c2389df2e9cb5d109f4
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2023-05-24 02:24:01 +0800

    Updated sql to match er diagram

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -10,6 +10,7 @@ CREATE TABLE skill_groups
 DROP TABLE IF EXISTS pure_colo_skill_names;
 CREATE TABLE pure_colo_skill_names
 (
+    art_unique_id INTEGER NOT NULL,
     jp_colo_skill_name VARCHAR NOT NULL,
     en_colo_skill_name VARCHAR NOT NULL,
     active_colour VARCHAR DEFAULT '#f598f2' NOT NULL,
@@ -17,7 +18,7 @@ CREATE TABLE pure_colo_skill_names
     updated_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'Etc/GMT+8'),
     CONSTRAINT fk_skill_type
         FOREIGN KEY (skill_type) REFERENCES skill_groups (skill_type),
-    PRIMARY KEY (jp_colo_skill_name)
+    PRIMARY KEY (art_unique_id)
 );
 
 DROP TABLE IF EXISTS element_attributes;
@@ -81,66 +82,54 @@ CREATE TABLE rarities
 DROP TABLE IF EXISTS colosseum_skills;
 CREATE TABLE colosseum_skills
 (
-    jp_colo_skill_name VARCHAR NOT NULL,
+    art_mst_id INTEGER NOT NULL,
+    art_unique_id INTEGER NOT NULL,
     jp_colo_skill_desc TEXT NOT NULL,
     prep_time SMALLINT NOT NULL,
     effective_time SMALLINT NOT NULL,
     jp_rank VARCHAR NOT NULL,
     en_colo_skill_desc TEXT NOT NULL,
+    colo_sp SMALLINT NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'Etc/GMT+8'),
     CONSTRAINT fk_jp_skill_name
-        FOREIGN KEY (jp_colo_skill_name) REFERENCES pure_colo_skill_names (jp_colo_skill_name) ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY (art_unique_id) REFERENCES pure_colo_skill_names (art_unique_id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT fk_skill_rank
         FOREIGN KEY (jp_rank) REFERENCES ranks (jp_rank) ON DELETE CASCADE ON UPDATE CASCADE,
-    PRIMARY KEY (jp_colo_skill_name, jp_rank)
+    PRIMARY KEY (art_mst_id)
 );
 
 
 DROP TABLE IF EXISTS nightmares;
 CREATE TABLE nightmares
 (
+    card_mst_id INTEGER NOT NULL,
+    art_mst_id INTEGER NOT NULL,
     jp_name VARCHAR NOT NULL,
     en_name VARCHAR NOT NULL,
     jp_icon_url TEXT NOT NULL,
     en_icon_url TEXT NOT NULL,
-    jp_colo_skill_name VARCHAR NOT NULL,
     jp_rank varchar NOT NULL,
     attribute_id SMALLINT NOT NULL,
     rarity_id SMALLINT NOT NULL,
-    colo_sp SMALLINT NOT NULL,
     global BOOLEAN NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'Etc/GMT+8'),
     CONSTRAINT fk_nightmare_skill
-        FOREIGN KEY (jp_colo_skill_name, jp_rank) REFERENCES colosseum_skills (jp_colo_skill_name, jp_rank) ON DELETE CASCADE ON UPDATE CASCADE,
+        FOREIGN KEY (art_mst_id) REFERENCES colosseum_skills (art_mst_id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT fk_nightmare_attribute
         FOREIGN KEY (attribute_id) REFERENCES element_attributes (attribute_id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT fk_nightmare_rarity
         FOREIGN KEY (rarity_id) REFERENCES rarities (rarity_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    PRIMARY KEY (jp_name, rarity_id)
+    PRIMARY KEY (card_mst_id)
 );
-/*
-DROP TABLE IF EXISTS nightmare_tag_relations;
-CREATE TABLE nightmare_tag_relations
-(
-    tag_id SMALLINT NOT NULL,
-    jp_name VARCHAR NOT NULL,
-    rarity_id SMALLINT NOT NULL,
-    CONSTRAINT fk_nightmare_relation_tag
-        FOREIGN KEY (tag_id) REFERENCES tags (tag_id),
-    CONSTRAINT fk_nightmare_relation_nightmare
-        FOREIGN KEY (jp_name, rarity_id) REFERENCES nightmares (jp_name, rarity_id) ON DELETE CASCADE ON UPDATE CASCADE,
-    PRIMARY KEY (tag_id, jp_name, rarity_id)
-);
-*/
 
 DROP TABLE IF EXISTS skill_tag_relations;
 CREATE TABLE skill_tag_relations
 (
     sub_tag_id SMALLINT NOT NULL,
-    jp_colo_skill_name VARCHAR NOT NULL,
+    art_unique_id INTEGER NOT NULL,
     CONSTRAINT fk_skill_relation_tag
         FOREIGN KEY (sub_tag_id) REFERENCES sub_categories (sub_tag_id),
     CONSTRAINT fk_skill_relation_skill  
-        FOREIGN KEY (jp_colo_skill_name) REFERENCES pure_colo_skill_names (jp_colo_skill_name) ON DELETE CASCADE ON UPDATE CASCADE,
-    PRIMARY KEY (sub_tag_id, jp_colo_skill_name)
+        FOREIGN KEY (art_unique_id) REFERENCES pure_colo_skill_names (art_unique_id),
+    PRIMARY KEY (sub_tag_id, art_unique_id)
 );
