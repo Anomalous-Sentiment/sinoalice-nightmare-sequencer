e2e9c751ff6241f57ab7e0583f932ded50eafbeb
Anomalous Sentiment <63189795+Anomalous-Sentiment@users.noreply.github.com>
2023-05-24 03:08:56 +0800

    Added proper function stubs

--- sinoalice-nightmare-sequencer/new_utils.js
+++ sinoalice-nightmare-sequencer/new_utils.js
@@ -17,13 +17,22 @@ async function updateDatabase()
 {
     try {
       // Get jp art list json
+      // https://raw.githubusercontent.com/sinoalice-datamine/data/master/JP/art_mst_list.json
+      const jpArtPromise = fetch('https://raw.githubusercontent.com/sinoalice-datamine/data/master/JP/art_mst_list.json').then(res => res.json())
 
       // Get en art list json
+      const enArtPromise = fetch('https://raw.githubusercontent.com/sinoalice-datamine/data/master/EN/art_mst_list.json').then(res => res.json())
 
       // get jp card list json
+      // https://raw.githubusercontent.com/sinoalice-datamine/data/master/JP/card_mst_list.json
+      const jpCardPromise = fetch('https://raw.githubusercontent.com/sinoalice-datamine/data/master/JP/card_mst_list.json').then(res => res.json())
 
       // get en card list json
+      const enCardPromise = fetch('https://raw.githubusercontent.com/sinoalice-datamine/data/master/EN/card_mst_list.json').then(res => res.json())
 
+      // Wait for all fetches to complete
+      const [jpArtList, jpCardList, enArtList, enCardList] = Promise.all([jpArtPromise, jpCardPromise, enArtPromise, enCardPromise])
+      
       // Construct a json obj with en skill ranks as keys and jp skill ranks as values (To convert en skill ranks into their equivalent jp version)
 
       // Construct a pure colo skill list with en and jp art lists (using unique_art_id, en name, and jp name)
@@ -31,9 +40,33 @@ async function updateDatabase()
       // Format jp card list into a form suitable to insert into db
 
       // Format en card list into a form suitable to insert into db (use the rank json obj to help)
+
+      // Insert all into database
+      
       }
       catch(err)
       {
         console.log(err)
       }
 }
+
+function createEnToJpRankMap(jpArtList, enArtList)
+{
+
+}
+
+function mapEnRanksToJp(enCardList)
+{
+
+}
+
+function createUniqueSkillList(artList)
+{
+
+}
+
+function createNightmareList(cardList)
+{
+
+}
+
